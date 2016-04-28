44a98990fa051c16d45a1642af198228785da818
Timo Würsch <twuersch@gmail.com>
2016-04-28 11:07:21 +0200

    Flexibler gemacht und Decimal integriert.

--- accounting.py
+++ accounting.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import csv
-positionen = []
-dateiname = "filename.csv"
+from decimal import Decimal
 
-with open(dateiname) as csvfile:
-    reader = csv.DictReader(csvfile, delimiter='\t')
-    for position in reader:
-        positionen.append(position)
+def positionen_lesen(dateiname, delimiter="\t"):
+  positionen = []
+  csv.register_dialect("own", delimiter=delimiter)
+  with open(dateiname) as csvfile:
+      reader = csv.DictReader(csvfile, dialect="own")
+      for position in reader:
+          positionen.append(position)
+  return positionen
 
 def total(positionen):
-    return reduce(lambda summe, position: summe + float(position['Betrag']), positionen, 0.0)
+    return reduce(lambda summe, position: summe + Decimal(position['Betrag']), positionen, Decimal("0"))
