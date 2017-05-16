661f1cc2ad33772d3a891664ccd2f8b7fde92aa6
Timo Würsch <twuersch@gmail.com>
2017-05-16 13:01:30 +0200

    added example csv and function diff_percent.

--- accounting.py
+++ accounting.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv
-positionen = []
-dateiname = "filename.csv"
+"""
+Load and drop into REPL:
 
-with open(dateiname) as csvfile:
-    reader = csv.DictReader(csvfile, delimiter='\t')
-    for position in reader:
-        positionen.append(position)
+python -i accounting.py
+ipython -i accounting.py
+"""
+
+import csv
+
+def readCSV(filename):
+    """
+    Reads the given CSV file into a twodimensional list, rows first, columns second.
+    """
+    positionen = []
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='\t')
+        for position in reader:
+            positionen.append(position)
+    return positionen
+    
 
 def total(positionen):
+    """
+    Sums up the items' 'Betrag' field.
+    """
     return reduce(lambda summe, position: summe + float(position['Betrag']), positionen, 0.0)
+
+
+def diff_percent(v0, v1):
+    """
+    Returns the difference from v0 to v1 in percent.
+    """
+    return (float(v1)-float(v0))/float(v0)*100.0
